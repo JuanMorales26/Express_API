b40ff2c7d641843c4d013ed971fc20d8668344bb
Juan Morales <launchx10261@innovaccion.mx>
2022-04-28 01:13:29 -0500

    Agregando exitosamente las pruebas para Get

--- test/app.test.js
+++ test/app.test.js
@@ -5,13 +5,49 @@ describe("Unit test for Express_API_HTTP_Methods", () => {
     test("GET /v1/explorers", (done) => {
       request(app)
         .get("/v1/explorers")
+        .expect("Content-Type", /json/)
         .expect(200)
         .expect((res) => {
-            expect(res).toBe('Culo')
+            expect(res.body[0].id).toBe(1)
+            expect(res.body[0].name).toBe("Juan1")
+            expect(res.body[1].id).toBe(2)
+            expect(res.body[1].name).toBe("Juan2")
+            expect(res.body[2].id).toBe(3)
+            expect(res.body[2].name).toBe("Juan3")
         })
         .end((err, res) => {
           if (err) return done(err);
           return done();
         })
     })
+
+    test("GET /v1/explorers/:id", (done) => {
+        request(app)
+          .get("/v1/explorers/1")
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.id).toBe(1)
+          })
+          .end((err, res) => {
+            if (err) return done(err);
+            return done();
+          })
+      })
+
+     /** test("Post /v1/explorers", (done) => {
+        request(app)
+          .post("/v1/explorers")
+          .expect("Content-Type", /json/)
+          .send({body:"Cliente_Nuevo"})
+
+          .expect(201)
+          .expect((res) => {
+              expect(res.body).toBe("Cliente_Nuevo")
+          })
+          .end((err, res) => {
+            if (err) return done(err);
+            return done();
+          })
+      })*/
 })
