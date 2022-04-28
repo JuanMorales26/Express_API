42550c05696e82f1046500385f22918883617a11
Juan Morales <launchx10261@innovaccion.mx>
2022-04-27 20:11:21 -0500

    Agregando POST Crea un endpoint que se encargue de crear un explorer y  PUT Crea un endpoint que se encargue de actualizar un explorer

--- app.js
+++ app.js
@@ -39,6 +39,17 @@ app.get('/v1/explorers/:id', (req, res)=>{
     res.status(200).json(explorer1)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //Parámetros de un cliente 
+    res.status(201).json({message: "Created"})
+})
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parámetros de un cliente 
+    res.status(200).json({message: "Updated!"})
+})
 //Con esto inicializamos esta app
 app.listen(port,() =>{
     console.log(`Example app listening on port ${port}`)
